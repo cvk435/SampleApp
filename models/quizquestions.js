3d6ee017991fed59bbe601473696a8cd91cde942
nareshbabu.nookala@devrabbit.com <nareshbabu.nookala@devrabbit.com>
2018-04-20 23:41:29 +0530

    dsa

--- models/quizquestions.js
+++ models/quizquestions.js
@@ -10,7 +10,7 @@ var  authentication= {
             if(rows.length==0){
                 console.log(data,'test')
           
-                db.query("insert Questions set Question=?,OptionA=?,OptionB=?,OptionC=?,RightAnswer=?,NoViews=?,status=?,Subject=?,PostedBy=?,postedDate=?,ModifiedDate=?,ModifiedBy=?",[data.question,data.optionA,data.optionB,data.optionC,data.rightAnswer,0,'',data.subject,data.userId,data.postedDate,'',''],function(err,res){
+                db.query("insert Questions set Question=?,OptionA=?,OptionB=?,OptionC=?,RightAnswer=?,NoViews=?,status=?,Subject=?,PostedBy=?,postedDate=?",[data.question,data.optionA,data.optionB,data.optionC,data.rightAnswer,0,'',data.subject,data.userId,data.postedDate],function(err,res){
      
             if(res.affectedRows==1){
                 callback({status:200,messsage:'inserted successfully'})
